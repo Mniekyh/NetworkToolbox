025f9ef85f058c04621516cb8c92b3dfb47712f8
Mniekyh <m.v.kowalski@gmail.com>
2026-05-24 13:37:36 +0200

    Pinger works, but now needs to grab it in one tool

--- core/pinger.py
+++ core/pinger.py
@@ -1,5 +1,6 @@
 
-from core.hosts import Host # from core/hosts take class Host
+from hosts import Host # from core/hosts take class Host
+from hosts import load_hosts
 import platform
 from dataclasses import dataclass 
 import subprocess
@@ -19,11 +20,21 @@ def ping_host(host:Host): # function to check connection
         systemOsFlag = "-n"
 
     result = subprocess.run(["ping", systemOsFlag,"5",host.address], capture_output=True, text=True) # run subprocess to check connection
-            lines = result.stdout.splitlines() #get block of text, and cut it to lines
-
-            if not lines or lines.startswith("#"): #if comment or empty line, continue
-                continue
-
-            parts = lines.split() #cut line into pieces
-            desitnation = parts[0]
-            ttl = 
+    lines = result.stdout.splitlines() #get block of text, and cut it to lines
+    
+  
+    for line in lines:
+        if "from" in line:
+            parts = line.split() #cut line into pieces
+            desitnation = parts[3]
+            ttl = parts[5].split("=")[1]
+            time = parts[6].split("=")[1]
+            print("Time to kill:",ttl)
+        elif "transmitted" in line:
+            parts = line.split()
+            transmitedPackets = parts[0]
+            receivedPackets = parts[3]
+            percentOfLossPackets = parts[5][:-1]
+            print("Transmitted packets in shot: ",transmitedPackets)
+host = load_hosts("../hosts.txt")
+print(ping_host(host[3]))
--- main.py
+++ main.py
@@ -1,1 +1,37 @@
 print("Heloł")
+
+#import packages
+from dataclasses import dataclass, field
+from pathlib import Path
+import subprocess
+
+#Begin coding
+#Create class Host, which is responsible for one host from list
+@dataclass
+class Host
+ address: str #required address of host
+ label: str = "" # nice to have label for this host, as it will give more info whose IP it is
+
+ def display_name(self) -> str: #creating nice label, if not exist, then show ip address
+    return self.label if self.label else self.address
+
+def load_hosts(path: str | Path) ->list[Host]: # load hosts from file, and put them into list
+    hosts = []
+    filepath = Path(path)
+
+    if not filepath.exists(): #If no hosts file was found, return error
+        raise FileNotFoundError (f"No host file was found in: {filepath}")
+
+    with filepath.open("r", encoding="utf-8") as openedHosts: # if it was possible to open hosts file, open it as OpenedHosts
+        for line in openedHosts: #for each object(verse) in file
+            line = line.strip # strip line for words
+
+            if not line or line.startswith('#'): #if empty line (like enter) or its comment, ignore and go to next verse
+                continue
+
+            parts = line.split() #split line to words
+            address = parts[0] #take first element of parts, and lock it in variable address
+            label = parts[1] if len(parts) > 1 else ""
+            hosts.append(Host(address=address, label=label))
+
+    return hosts
